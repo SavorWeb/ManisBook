ba8a21195351404248f779760a07e00ae9139e1c
Ryan Maskell <Ryan@MaskellMedia.com>
2014-06-05 16:13:54 -0700

    masonry

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
 		<title>The Street Agent | MANIS</title>
 		<link rel="stylesheet" type="text/css" href="_/fonts/bebas/stylesheet.min.css">
+		<link rel="stylesheet" type="text/css" href="_/plugins/fancybox/source/jquery.fancybox.min.css">
 		<link href="_/css/screen.min.css" rel="stylesheet" type="text/css" media="all"/>
 
 		<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
@@ -52,22 +53,26 @@ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque vo
 	</div>
 </div>
 <div id="gallery" class="editable-gallery row top-shim bottom-shim">
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
-	<a href="" class="fancybox-effects-d"><img src="http://www.placehold.it/500x500" alt=""></a>
+	<a href="media/photos/aryansubpoena.jpeg" class="fancybox-effects-d"><img src="media/photos/aryansubpoena.jpeg" alt=""></a>
+	<a href="media/photos/firearms-training.jpeg" class="fancybox-effects-d"><img src="media/photos/firearms-training.jpeg" alt=""></a>
+	<a href="media/photos/frankiemarconi.jpg" class="fancybox-effects-d"><img src="media/photos/frankiemarconi.jpg" alt=""></a>
+	<a href="media/photos/hustle.jpeg" class="fancybox-effects-d"><img src="media/photos/hustle.jpeg" alt=""></a>
+	<a href="media/photos/tonymagliano.jpg" class="fancybox-effects-d"><img src="media/photos/tonymagliano.jpg" alt=""></a>
+	<a href="media/photos/tonymagliano1979.jpg" class="fancybox-effects-d"><img src="media/photos/tonymagliano1979.jpg" alt=""></a>
+	<a href="media/photos/tonymagliano1981.jpg" class="fancybox-effects-d"><img src="media/photos/tonymagliano1981.jpg" alt=""></a>
+	<a href="media/photos/tonymagliano1982.jpg" class="fancybox-effects-d"><img src="media/photos/tonymagliano1982.jpg" alt=""></a>
+	<a href="media/photos/waynejordan-booking.jpg" class="fancybox-effects-d"><img src="media/photos/waynejordan-booking.jpg" alt=""></a>
+	<a href="media/photos/WFM_butch.jpg" class="fancybox-effects-d"><img src="media/photos/WFM_butch.jpg" alt=""></a>
+	<a href="media/photos/WFM_guns.jpg" class="fancybox-effects-d"><img src="media/photos/WFM_guns.jpg" alt=""></a>
+	<a href="media/photos/wfm_marinecorps.jpg" class="fancybox-effects-d"><img src="media/photos/wfm_marinecorps.jpg" alt=""></a>
 </div>
 <div class="author top-shim">
 	<div class="row">
 		<div class="mugshot colspan12-4 as-grid">
 			<img src="_/img/wayne.png" alt="">
 			<div class="typeset">
-			<h3>Wayne Manis</h3>
-			<p>Ask A Question</p>
+				<h3>Wayne Manis</h3>
+				<a href="#">Ask A Question</a>
 			</div>
 		</div>
 		<div class="colspan12-8 as-grid with-gutter typeset">
@@ -80,13 +85,73 @@ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque vo
 </div>
 <div class="cta">
 	<div class="row typeset">
-		<p>Order the book in hardback or ebook on Amazon Books!</p>
-		<a href="" class="btn">Pre-Order</a>
+		<p>Order the book in hardback or ebook on Amazon Books! 		<a href="" class="btn">Pre-Order</a></p>
 	</div>
 </div>
+<footer>
+	<div class="row">
+		<a class="logo" href="/"><img src="_/img/manis-logo.png" alt=""></a>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, ducimus, quidem ratione iure soluta maxime dolorem!</p>
+
+		<div class="social">
+			<a href="#" class="social-btns"><img src="_/img/facebook.png" alt=""></a>
+			<a href="#" class="social-btns"><img src="_/img/twitter.png" alt=""></a>
+			<a href="#" class="social-btns"><img src="_/img/linkedin.png" alt=""></a>
+		</div>
+	</div>
+</footer>
 
 		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+<!-- Add fancyBox -->
+<link rel="stylesheet" href="_/plugins/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+<script type="text/javascript" src="_/plugins/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+
+<!-- Optionally add helpers - button, thumbnail and/or media -->
+<link rel="stylesheet" href="_/plugins/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+<script type="text/javascript" src="_/plugins/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+<script type="text/javascript" src="_/plugins/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+
+<link rel="stylesheet" href="_/plugins/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+<script type="text/javascript" src="_/plugins/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
+<script src="_/js/masonry.pkgd.min.js"></script>
 		<script src="_/js/script.min.js"></script>
+		<script>
+$(document).ready( function() {
+    // Don't execute if we're in the Live Editor
+    if( !window.isCMS ) {
+        // Group images by gallery using `data-fancybox-group` attributes
+        var galleryId = 1;
+        $('.editable-gallery').each( function() {
+            $(this).find('a').attr('data-fancybox-group', 'gallery-' + galleryId++);
+        });
+        // Initialize Fancybox
+        $('.editable-gallery a').fancybox({
+            // Use the `alt` attribute for captions per http://fancyapps.com/fancybox/#useful
+            beforeShow: function() {
+                var alt = this.element.find('img').attr('alt');
+                this.inner.find('img').attr('alt', alt);
+                this.title = alt;
+            }
+        });
+    }
+});
+		</script>
+		<script>
+		$(document).ready( function() {
+var container = document.querySelector('#gallery');
+var anchorimg = $('#gallery a');
+
+var msnry = new Masonry( container, {
+  columnWidth: 50,
+  itemSelector: '.fancybox-effects-d'
+});
+
+$anchorimg.imagesLoaded( function() {
+  $container.masonry();
+});
+});
+		</script>
 
 	</body>
 
