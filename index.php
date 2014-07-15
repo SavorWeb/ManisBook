ee2e106c4fb99502c4985a327710ff8e41452dec
Ryan Maskell <ryan@savorweb.com>
2014-07-15 12:16:58 -0700

    Modal and new content

--- index.php
+++ index.php
@@ -15,6 +15,7 @@
 		<link href="_/css/screen.min.css" rel="stylesheet" type="text/css" media="all"/>
 
 		<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+
 		<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
 	</head>
 
@@ -27,7 +28,7 @@
 
 			<nav>
 				<ul>
-					<li><a href="/Pre-Order">Pre-Order</a></li>
+					<li><a target="_blank" href="http://www.amazon.com/The-Street-Agent-Infiltrator-Terrorists/dp/1933909609/ref=sr_1_1?ie=UTF8&qid=1405357767&sr=8-1&keywords=The+Street+Agent">Pre-Order</a></li>
 				</ul>
 			</nav>
 	</section>
@@ -38,19 +39,22 @@
 <div class="row top-shim">
 	<div class="typeset colspan12-6 as-grid">
 		<h1>The Book</h1>
-		<p>The street agent is the gripping tale of the need for a synopsis to go here detailing aspects about the book. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-</p>
-<p>
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.
-</p><p>
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis. Suspendisse urna nibh, viverra non, semper suscipit, posuere a, pede.</p>
+		<p>
+			Within the FBI, the street agent is known as the heart of the Bureau. The street agent discovers and detects criminal activity then decides how best to attack it. Once the target is defined and a plan is formulated, the street agent hits the bricks and systematically dismantles and destroys the criminal enterprise. Wayne F. Manis was known as a street agent and few agents in the history of the FBI have traveled as diverse a course as he did during his career. On the extreme left, he was undercover with the violent faction of The Weather Underground. On the extreme right, he worked with the Ku Klux Klan and investigated the Aryan Nations. And somewhere in between was the Mafia and their related organized crime figures. As a team leader of an FBI SWAT Team, he engaged bank robbers, fugitives and terrorists in armed confrontations. He participated in his share of shootings and killings. These were not killings of anonymous or unfamiliar persons but people he knew. Some were people in his community where he knew members of their family. These were people he pursued and investigated for years. Their stories intertwine with his. These stories are personal to the author and compelling.</p>
+		<p>
+			Near the end of his career, Wayne Manis spearheaded the F.B.I.'s largest domestic terrorism investigation prior to the Oklahoma City Bombing. The case was directed at a white supremacist group known as The Order. He led a team of agents in the search for these terrorists who threatened the members of the U. S. Congress and sent them a formal Declaration of War. They assassinated their enemies, committed multiple bombings, and robberies, including a 3.6 million dollar robbery of a Brink s armored truck. 
+		</p>
 	</div>
 	<div class="colspan12-6 as-grid with-gutter">
 		<img class="hero-img" src="_/img/streetagent.png">
-		<p class="typeset">
-Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque volutpat mattis eros. Nullam malesuada erat ut turpis.</p>
+		<div class="typeset">
+		<p>With their stolen money, they purchased state of the art weaponry to engage the FBI in armed confrontations. This was a career case that other agents dream of but seldom get the opportunity to experience. For a period of time, this investigation was the #1 case in the FBI and the Director was briefed daily on its progress. As the case agent in charge of the investigation, The author lived this war between the FBI Agents and the terrorists and survived a fiery gun battle in a secluded hideout with the terrorist group s leader.</p>
+
+		<p>The Street Agent contains selected stories in which Wayne Manis was the case agent, the SWAT Team Leader, or the undercover agent. Events are related through the author's eyes and as they unfolded in his presence. A day as a street agent included terrorists, Klansmen, mobsters, and murderers. Wayne F. Manis is now able to tell the tale of these incredible cases. In this fascinating no-holds-barred book, the public gets a view inside the FBI at the street level where courageous men and women dedicate themselves to the never-ending war on crime.</p>
+		</div>
 	</div>
 </div>
+<div class="row"><div class="colspan12-12 as-grid"><a class="btn" style="margin: 15px auto 0; text-align: center; max-width: 300px; display: block;" href="#excerpt">Read some Excerpts from the book</a></div></div>
 <div id="gallery" class="editable-gallery row top-shim bottom-shim">
 	<a href="media/photos/aryansubpoena.jpeg" class="fancybox-effects-d"><img src="media/photos/aryansubpoena.jpeg" alt=""></a>
 	<a href="media/photos/firearms-training.jpeg" class="fancybox-effects-d"><img src="media/photos/firearms-training.jpeg" alt=""></a>
@@ -71,36 +75,76 @@ Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec odio. Quisque vo
 			<img src="_/img/wayne.png" alt="">
 			<div class="typeset">
 				<h3>Wayne Manis</h3>
-				<a href="#">Ask A Question</a>
+				<a href="mailto:thestreetagent@onbelaysecurity.com">Ask A Question</a>
 			</div>
 		</div>
 		<div class="colspan12-8 as-grid with-gutter typeset">
 			<h1>The Author</h1>
-			<p>Wayne is well known and needs content here in the form of a short biography. Modi, praesentium, excepturi, natus suscipit consequatur facilis ab quis debitis porro ducimus expedita quibusdam voluptate animi maxime blanditiis cupiditate culpa fugiat dolores!</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet, cumque, omnis possimus ab odit perferendis eos quis soluta consequuntur praesentium voluptas iusto animi labore! Beatae, velit neque ipsa nostrum perferendis.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, optio, vero, possimus, harum illum facilis eveniet ea quisquam voluptatibus temporibus magnam nam assumenda consectetur minima eum soluta explicabo nisi nulla.</p>
+			<p>Wayne Manis served for more than 28 years as a Special Agent in the Federal Bureau of Investigation. Preceding the FBI, Wayne was a United States Marine Corps Captain and acting Provost Marshal in charge of the Criminal Division of Forced Troops, Fleet Marine Force Pacific. During Wayne's FBI career, he spearheaded the FBI s undercover program being the first agent to go undercover following WW ll. While working undercover, Wayne penetrated the violent factions of the New Left in Chicago, posed as a hit man in a mob related murder for hire case and infiltrated the notorious criminal empire of the New Orleans Crime Family after receiving the blessing from the family s Godfather, Carlos Marcello.</p>
+
+			<p>Wayne also served as a team leader of an elite FBI SWAT Team and was engaged in several deadly shootouts while serving in this capacity. Later, Wayne was the lead case agent for the nation's largest domestic terrorism case prior to the Oklahoma City bombing against a white supremacist group known as "The Order". Wayne has been featured in several episodes of the T.V. series, FBI-The Untold Stories, Turning Point produced by ABC, Discovery Channel s Angry America, and The FBI Files. He has been a news commentator for CNN and a speaker on Domestic Terrorism throughout the United States and Canada. His career and casework have historical implications and still reverberate into many present day events.</p>
+			<a class="btn" href="#onbelay">Learn about Wayne's new Business</a>
 		</div>
 	</div>
 </div>
 <div class="cta">
 	<div class="row typeset">
-		<p>Order the book in hardback or ebook on Amazon Books! 		<a href="" class="btn">Pre-Order</a></p>
+		<p>Order the book today in hardback cover on Amazon Books! 		<a target="_blank" href="http://www.amazon.com/The-Street-Agent-Infiltrator-Terrorists/dp/1933909609/ref=sr_1_1?ie=UTF8&qid=1405357767&sr=8-1&keywords=The+Street+Agent" class="btn">Pre-Order</a></p>
 	</div>
 </div>
 <footer>
 	<div class="row">
 		<a class="logo" href="/"><img src="_/img/manis-logo.png" alt=""></a>
-		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, ducimus, quidem ratione iure soluta maxime dolorem!</p>
+		<p>"The Street Agent" by Wayne F. Manis is proudly published by <a style="color:#fff" target="_blank" href="http://historypublishingco.com/authors/manis/streetagent.php">The History Publishing Company</a>. All rights reserved ®2014</p>
 
 		<div class="social">
-			<a href="#" class="social-btns"><img src="_/img/facebook.png" alt=""></a>
-			<a href="#" class="social-btns"><img src="_/img/twitter.png" alt=""></a>
-			<a href="#" class="social-btns"><img src="_/img/linkedin.png" alt=""></a>
+			<a target="_blank" href="https://www.facebook.com/thestreetagent" class="social-btns"><img src="_/img/facebook.png" alt=""></a>
+			<a target="_blank" href="https://twitter.com/WayneFManis" class="social-btns"><img src="_/img/twitter.png" alt=""></a>
+			<a target="_blank" href="http://www.linkedin.com/pub/wayne-manis/44/795/b88/" class="social-btns"><img src="_/img/linkedin.png" alt=""></a>
 		</div>
 	</div>
 </footer>
 
+	<div class="remodal typeset" data-remodal-id="excerpt">
+		<h1>Excerpts</h1>
+		<p>Working as a street agent in the FBI was an adventure that led me through some interesting times. I spent days living out of a Bureau car conducting mobile surveillances.  I captured ruthless murdering gangsters, masked villains from the secret order of the Ku Klux Klan, and sophisticated white-collar criminals. I spent years undercover travelling with the violent factions of the Weather Underground, the Mafia, and other organized criminal entities. I pursued and arrested radical terrorists engaged in armed robberies, assassinations and bombings.  I experienced the ‘thrill of the chase,’ the immense danger of armed confrontation, and the infinite satisfaction of being the leader of a highly trained SWAT team. I felt desperation and isolation after being arrested and incarcerated under an assumed identity while undercover.
+		</p><p>
+In this career, there were some painful experiences. I survived several fights and a severe beating while undercover. I was stabbed during an arrest and narrowly escaped being shot while in a gun battle with a terrorist wielding a machine gun. I participated in my share of violence, but I was never involved in the killing of anyone that didn’t need killing.</p><hr><p>
+
+The 1968 Democratic National Convention was set to take place in Chicago. We in the FBI feared that something bad was about to occur.  Informants had reported that that buildings in Chicago were going to be bombed, police assassinated and Selective Service Boards invaded by armed protesters. They informed us that convention delegates would be assaulted while Vice President Hubert Humphrey, Chicago Mayor Richard Dailey, Senator Eugene McCarthy were be targeted for assassination.</p><p>
+I penetrated the violent factions of the ‘New Left’ to subvert their planned actions of violence.  I needed to identify the conspirators and acquire valuable intelligence regarding the contemplated assassinations. It was a heavy task and an awesome responsibility but I savored the job at hand and proceeded into the anarchist’s maze full steam ahead.</p><hr><p>
+
+Klansmen of the Klavern of the Knights of the Ku Klux Klan had committed the first Klan-related kidnapping in more than a decade.  The masked Klansmen drug a terrified woman by her hair through her living room, out into the street, and tossed her into a waiting car. It was a cold night in Alabama and she was dressed only in a thin nightgown. They drove her to a remote wooded location and tied her to a tree at which time one of the Klansmen pressed a razor sharp knife against the nape of her neck and drew it down her back cutting into the flesh and in turn cutting through the nightgown allowing it to fall at her feet. The robed Klansman lit a torch and produced a bullwhip. She was struck seven times with the whip at which time she collapsed. Her crime? She had dated a black man.</p><p>
+I knew that we would identify the Klansmen and justice would be served. What I did not know was that I would encounter considerable resistance from the local Chief of Police and we would expose several police officers that were involved in the Klan.</p> <hr><p>
+                                                                                    
+Carlos Marcello was the Mafia Boss of ‘The New Orleans Crime Family,’ the oldest Mafia family in the United States. He gained unwanted notoriety in political and law enforcement circles as his name is often mentioned in conspiracy theories surrounding the assassination of President John F. Kennedy.  Carlos knew Lee Harvey Oswald and had indirect connections to Jack Ruby. The Marcello family in New Orleans was powerful and his criminal enterprise was vast and Carlos himself was dangerous.   </p><p>
+This was foremost in my mind when I was escorted into Marcello’s office to receive his blessing.  As Frankie Marconi, a west coast gun for hire, I was to be his new enforcer to collect some dope debts and to assist Marcello with some strong-arm tactics. I needed to penetrate the criminal organization and build a ‘Historical Conspiracy’ case. The trail would lead me from New Orleans through Alabama, Georgia, Virginia, Mississippi, and finally Arizona. There would be gunplay along the way and there were many anxious moments.</p><hr><p>
+
+I often get asked how an undercover agent prepares for the criminal world.  When you’re undercover, there’s never a day off and you can never drop your guard.  You walk a tightrope every day, and at night, you sleep lightly.  There is no room for error.  This is the most difficult acting job imaginable.  No screenplay and no second-takes.  You get one chance to play the part convincingly.  You do it right the first time or you’re done. </p><p>
+There is a process one goes through before ‘going under’ each day.  For example, when meeting with mob bosses, I knew what to expect so I travelled clean – no gun, no wire, and no backup. Before you dress in the morning, you decide if you are wearing just a transmitter or if it’s going to be a ‘heavy day’ and you’re going to need the transmitter and the body recorder both.  You carefully attach them securely so they are well concealed and yet functional, and you then wear clothing that best accommodates the equipment.  Extra batteries for the transmitter are necessary, and they will need to be concealed as well.</p><p>
+Then you must decide whether you will pack a gun and determine where you plan to conceal it.  If the cops frisk you and they find the gun, they’ll book you for carrying a concealed weapon.  Once you reach the jail, they will find the transmitter and body recorder and there goes your cover.  These are things that add daily tension to every day of an undercover operation, and it is a tremendous relief to be with family and put these things aside for a while.  </p><hr><p>
+
+I began investigating a group of white supremacist domestic terrorists known as the ‘Silent Brotherhood’ in Coeur d’Alene, Idaho.  The group was led by a charismatic young man, Robert Mathews, who was on a path of crime to include assassinations, armored car robberies, counterfeiting and bombings. The goal was to commence a war against the United States Government and once accumulating a significant revolutionary force, they intended to force the government leaders to either step aside or join forces with them in a move to rid the U.S. of all Jews and Blacks. I would quickly be embroiled in a non-stop war that took every hour of every day and place my family in extreme risk.   </p><p>
+The Silent Brotherhood’s strength, their increasing numbers, their war chest and their weaponry grew. It was a ‘cat and mouse’ game at times with our paths crossing on several occasions. It became obvious to ‘The Silent Brotherhood’ and to the FBI Agents pursuing them, that a battle would soon take place. The only questions were when, where, and who would be the last man standing.</p><p>
+That question was answered on a cold wet December night at a remote cabin on Whidbey Island, Washington.  Other SWAT agents and I engaged the group’s leader, Robert Mathews, in a fiery machine gun battle that lasted throughout the night. SWAT Agents entered the cabin through the first floor and were inundated with a rain of gunfire as Matthews fired through the floorboards of the second floor where he had taken refuge.   By night’s end, the struggle would be over.  Matthew’s chose to die for his cause that night.  I’ve often thought, “Better him than me.”   
+ </p>
+
+</p>
+	</div>
+
+		<div class="remodal typeset" data-remodal-id="onbelay">
+		<h1>OnBelay Consulting Services</h1>
+<p>
+Wayne F. Manis founded Manis Investigations, Inc., in 1994 upon retirement from the FBI.  Manis has provided security services worldwide and has operated in several countries. His clientele includes several Fortune 500 companies, numerous well-known household brands, and a discrete number of private clients.  Manis Investigations has built a reputation for first class service and a wide range of capabilities utilizing an established network of retired FBI Agents in various capacities tailored to the client's individualized needs.</p>
+
+<p>Wayne F. Manis is excited to announce the evolution of Manis Investigations as it converges with OnBelay Security in an exciting new endeavor.  OnBelay will have the track record and experience of Wayne F. Manis and Manis Investigations plus offer cutting edge forensic and investigative techniques that are relevant in today's world of information gathering.</p>   
+
+<p>For more information see <a target="_blank" href="www.OnBelaySecurity.com">www.OnBelaySecurity.com</a>.</p>
+		</div>
+
 		<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+
+<script type="text/javascript" src="_/js/jquery.remodal.min.js"></script>
 
 <!-- Add fancyBox -->
 <link rel="stylesheet" href="_/plugins/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
