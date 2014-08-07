a85fa475d7f1ad46af50d892e8777ed1f2e0d920
Ryan Maskell <ryan@savorweb.com>
2014-08-07 12:06:55 -0700

    updates

--- 404.php
+++ 404.php
@@ -1,10 +1,43 @@
-<?php include("_/inc/head.php"); ?>
-	
-<?php include("_/inc/nav.php"); ?>
+<!doctype html>
+<!--[if lt IE 9 ]> <html lang="en-GB" class="no-js lt-ie9"> <![endif]-->
+<!--[if IE 9 ]> <html lang="en-GB" class="no-js ie9"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--> <html lang="en-GB" class="no-js"> <!--<![endif]-->
+	<head>
+		<meta charset="UTF-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="initial-scale=1, user-scalable=no">
+		<meta name="description" content="..." />
+		<meta name="keywords" content="Wayne Manis, Book, Undercover, Agent, Mob, Crime, Drama" />
 
-<section id="simple-page" class="row typeset editable">
-	<h1>Page not found</h1>
-	<p>It seems the page you were looking for isn't here... If you think it should be, <a href="mailto:contactus@jvwlaw.net?Subject=404%20page">let us know</a>. Otherwise, try going back to the <a href="/">home page</a>.</p>
-</section>
+		<title>404 | The Street Agent | MANIS</title>
+		<link rel="stylesheet" type="text/css" href="_/fonts/bebas/stylesheet.min.css">
+		<link rel="stylesheet" type="text/css" href="_/plugins/fancybox/source/jquery.fancybox.min.css">
+		<link href="_/css/screen.min.css" rel="stylesheet" type="text/css" media="all"/>
 
-<?php include("_/inc/foot.php"); ?>
+		<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+
+		<script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.7.1/modernizr.min.js"></script>
+	</head>
+
+	<body>
+
+	<header class="pagehead pullwidth">
+		<section class="row">
+
+			<a class="logo" href="/"><img src="_/img/manis-logo.png" alt=""></a>
+
+			<nav>
+				<ul>
+					<li><a target="_blank" href="http://www.amazon.com/The-Street-Agent-Infiltrator-Terrorists/dp/1933909609/ref=sr_1_1?ie=UTF8&qid=1405357767&sr=8-1&keywords=The+Street+Agent">Pre-Order</a></li>
+				</ul>
+			</nav>
+	</section>
+	<h1 class="hero">“404 Page not found.”
+</h1>
+</header>
+
+		<script src="_/js/script.min.js"></script>
+
+	</body>
+
+</html>
